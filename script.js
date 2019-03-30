f8885a71b78b681b1d1994061f9aef80ad06e8ac
Piotr Przygoda <przygoda@gmail.com>
2019-03-30 16:59:34 +0100

    Finished

---
+++ script.js
@@ -0,0 +1,71 @@
+var first = React.createClass({
+    getInitialState: function () { // początkowy stan komponentu
+        return {
+            counter: 0
+        };
+    },
+
+
+    componentDidUpdate: function () {
+        console.log('componentDidUpdate');
+    },
+
+    increment: function () {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
+
+    decrement: function () {
+        this.setState({
+            counter: this.state.counter - 1
+        });
+    },
+
+    render: function () {
+        return React.createElement('div', { className: 'counter' },
+            React.createElement('div', { id: 'counter' }, this.state.counter),
+            React.createElement('button', { onClick: this.increment }, ' + '),
+            React.createElement('button', { onClick: this.decrement }, ' - ')
+        );
+    }
+});
+
+var element = React.createElement(first);
+ReactDOM.render(element, document.getElementById('app'));
+ReactDOM.render(element, document.getElementById('counterTwo'));
+ReactDOM.render(element, document.getElementById('counterThree'));
+
+
+/*
+
+    componentWillMount: function() {
+        console.log('wykonuje się przed render');
+    },
+
+    componentDidMount: function() {
+        console.log('wyświetla komponenty na stronie');
+    },
+
+    componentWillReceiveProps: function() {
+        console.log('wykonuje się po dodaniu nowych propsów');
+    },
+
+    shouldComponentUpdate: function() {
+        console.log('sprawdza czy przerysowuje komponent');
+        return true;
+    },
+
+    componentWillUpdate: function() {
+        console.log('przygotowuje uaktualnienia');
+    },
+
+    componentDidUpdate: function() {
+        console.log('wyświetla komponent po uaktualnieniu');
+    },
+
+    componentWillUnmount: function() {
+        console.log('usuwa niepotrzebne komponenty');
+    },
+    
+    */
